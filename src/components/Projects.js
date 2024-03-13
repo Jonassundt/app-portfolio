e4808450746a5be9e6a94e097d0ebd10f64e2895
Jonas <jonassundt98@gmail.com>
2024-03-13 19:51:13 +0100

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,14 +3,14 @@ import ProjectCard from './ProjectCard';
 import proj_fengsel from "../images/proj_fengselDB.png";
 import proj_greyrock from "../images/proj_greyrock.png";
 import proj_hjemmekino from "../images/proj_hjemmekino.png";
-import proj_wordquiz from "../images/proj_wordquiz.png";
+// import proj_wordquiz from "../images/proj_wordquiz.png";
 
 
 const projects = [
     { name: "GreyRock", image: proj_greyrock, url: 'https://greyrock.onrender.com/' },
     { name: "Hjemmekino", image: proj_hjemmekino, url: "https://jonassundt.github.io/cinema/" },
     { name: "FengselDB", image: proj_fengsel, url: "" },
-    { name: "WordQuiz", image: proj_wordquiz, url: "https://jonassundt.github.io/WordQuiz/" }
+    // { name: "WordQuiz", image: proj_wordquiz, url: "https://jonassundt.github.io/WordQuiz/" }
 ];
 
 const Projects = () => {
